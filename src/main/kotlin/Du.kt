5d7021b6e67a6f23dd709bb7cb84cfd6cf970ca7
d1nee <d1neexoj@gmail.com>
2021-03-30 00:01:31 +0300

    fix

--- src/main/kotlin/Du.kt
+++ src/main/kotlin/Du.kt
@@ -1,7 +1,38 @@
 package du
+import java.io.IOException
+import java.nio.file.*
 
+import java.nio.file.attribute.BasicFileAttributes
+
+import java.util.concurrent.atomic.AtomicLong
 import java.io.File
 import java.util.*
+
+
+fun size(path: Path?): Long {
+    val size = AtomicLong(0)
+    try {
+        Files.walkFileTree(path!!, object : SimpleFileVisitor<Path>() {
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                size.addAndGet(attrs.size())
+                return FileVisitResult.CONTINUE
+            }
+
+            override fun visitFileFailed(file: Path, exc: IOException): FileVisitResult {
+                println("skipped: $file ($exc)")
+                return FileVisitResult.CONTINUE
+            }
+
+            override fun postVisitDirectory(dir: Path, exc: IOException): FileVisitResult {
+                println("had trouble traversing: $dir ($exc)")
+                return FileVisitResult.CONTINUE
+            }
+        })
+    } catch (e: IOException) {
+        throw AssertionError("walkFileTree will not throw IOException if the FileVisitor does not")
+    }
+    return size.get()
+}
 
 fun listFilesWithSubFolders(dir: File): ArrayList<File>? {
     val files = ArrayList<File>()
@@ -22,15 +53,14 @@ class Du(private val h: String, private val c: Boolean, private val si: Boolean,
         if ('.' !in iFile[0].toString()) {
             iterateList = listFilesWithSubFolders(iFile[0])!!.toMutableList()
         }
-
         val listForTest = mutableListOf<Int>()
         val resultList = mutableListOf<String>()
         val listForSum = mutableListOf<Int>()
-        var text:String
+        var text:Int
         var unit = h
 
         for (i in iterateList) {
-            text = i.bufferedReader().readText()
+            text = size(Paths.get(i.toString())).toInt()
             var kb = 1024
             var mb = 1024 * 1024
             var gb = 1024 * 1024 * 1024
@@ -41,21 +71,21 @@ class Du(private val h: String, private val c: Boolean, private val si: Boolean,
             }
             when (h) {
                 "KB" -> {
-                    listForSum.add(text.length / kb)
-                    resultList.add("Name: $i = ${(text.length / kb)} KB")
+                    listForSum.add(text / kb)
+                    resultList.add("Name: $i = ${(text / kb)} KB")
                 }
                 "MB" -> {
-                    listForSum.add(text.length / mb)
-                    resultList.add("Name: $i = ${(text.length / mb)} MB")
+                    listForSum.add(text / mb)
+                    resultList.add("Name: $i = ${(text/ mb)} MB")
                 }
                 "GB" -> {
-                    listForSum.add(text.length / gb)
-                    resultList.add("Name: $i = ${(text.length / gb)} GB")
+                    listForSum.add(text / gb)
+                    resultList.add("Name: $i = ${(text/ gb)} GB")
                 }
                 else -> {
                     unit = "B"
-                    listForSum.add(text.length)
-                    resultList.add("Name: $i = ${text.length} B")
+                    listForSum.add(text)
+                    resultList.add("Name: $i = ${text} B")
                 }
             }
         }
